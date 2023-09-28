df91cf84e71f93558754e07c9ce54666cb24029c
yasminsilva30 <133078071+yasminsilva30@users.noreply.github.com>
2023-09-28 11:24:30 -0300

    Add files via upload

---
+++ questão 4.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+main(){
+
+    int vetor[6], x[1], y[1], soma;
+
+    for (int i = 0; i < 6; i++) {
+        printf("Digite o numero: \n");
+        scanf("%d", &vetor[i]);
+    }
+
+    for (int i = 0; i < 6; i++) {
+        printf("\n Numero inserido: %d", vetor[i]);
+        printf("\n Valor da soma entre X e Y: %d", soma);
+    }
+
+}
